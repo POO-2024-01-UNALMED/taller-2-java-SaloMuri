09ec7616b030e6d5a514f6d66ab3a1fd4014412e
Salomé Murillo Gaviria <samurillog@unal.edu.co>
2024-02-16 21:37:54 -0500

    Cuarto evento
    Actualización clase Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,5 +1,39 @@
 package test;
 
 public class Auto {
-
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int numeroAsientos = 0;
+		for (int i = 0 ; i < asientos.length ; i++) {
+			if (asientos[i] != null) {
+				numeroAsientos++;
+			}
+		}
+		return numeroAsientos;
+	}
+	
+	String verificarIntegridad() {
+		String respuesta = null;
+		if (this.registro == this.motor.registro) {
+			for (int i = 0 ; i < asientos.length ; i++) {
+				if (asientos[i] != null) {
+					if (asientos[i].registro == registro) {
+						respuesta = "Auto original";
+					}
+					else {
+						respuesta = "Las piezas no son originales";
+					}
+				}
+			}
+			respuesta = "Auto original";
+		}
+		return respuesta;
+	}	
 }
