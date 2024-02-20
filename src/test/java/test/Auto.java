5cc67ba64aa3d01882d02fc39b1d29d1bd75a197
Salomé Murillo Gaviria <samurillog@unal.edu.co>
2024-02-19 19:55:51 -0500

    Octavo evento
    Cambio estético.

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -23,13 +23,11 @@ public class Auto {
 		if (this.registro != this.motor.registro) {
 			return "Las piezas no son originales";
 		}
-
 		for (int i = 0; i < asientos.length; i++) {
 		    if (asientos[i] != null && asientos[i].registro != this.registro) {
 		        return "Las piezas no son originales";
 		    }
 		}
-
 		return "Auto original";
 	}
 }
